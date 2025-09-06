3e2946696b5e04110b461d3103df99224aebfdbe
Paul Nguyen <paulnguyen@ucsb.edu>
2025-09-05 22:15:35 -0700

    Create Example.cpp

---
+++ Introduction/Example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Here is some text.";
+    return 0;
+}
+
+// This is a single-line comment
+
+/*
+ This is a multi-line comment.
+It can span across several lines
+to provide more detailed explanations.
+*/
+
+// Best practice: Name the first/primary source code file in each program main.cpp. This makes it easy to determine which source code file is the primary one.
